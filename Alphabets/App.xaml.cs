c9c8c21861b254448678b1332230c9a8c11cbe85
James Leahy <defuncart@gmail.com>
2019-03-07 19:02:51 +0100

    Added Xam.Plugins.Settings and UserSettings wrapper. On first launch the user's device language is saved.

--- Alphabets/App.xaml.cs
+++ Alphabets/App.xaml.cs
@@ -1,6 +1,8 @@
-﻿using Alphabets.Views;
+﻿using Alphabets.Helpers;
 using Alphabets.Resources;
+using Alphabets.Views;
 using Plugin.Multilingual;
+using System.Linq;
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 
@@ -14,8 +16,15 @@ namespace Alphabets
             //initialize all components
             InitializeComponent();
 
-            //setup localization
-            CrossMultilingual.Current.CurrentCultureInfo = CrossMultilingual.Current.DeviceCultureInfo;
+            //if this is the first launch, try to initialize the display language to be the same as the device language
+            if (!UserSettings.AlreadyLaunched)
+            {
+                UserSettings.Language = CrossMultilingual.Current.DeviceCultureInfo.TwoLetterISOLanguageName; //if the device language is invalid, the default ("en") will be kept
+                UserSettings.AlreadyLaunched = true;
+            }
+
+            //update localization depending on user's setting
+            CrossMultilingual.Current.CurrentCultureInfo = CrossMultilingual.Current.NeutralCultureInfoList.ToList().First(element => element.TwoLetterISOLanguageName == UserSettings.Language);
             AppResources.Culture = CrossMultilingual.Current.CurrentCultureInfo;
 
             //set the initial page
---
+++ Alphabets/Helpers/Localization.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Alphabets.Helpers
+{
+    /// <summary>
+    /// A helper class for localization.
+    /// </summary>
+    public static class Localization
+    {
+        /// <summary>
+        /// An array of valid languages.
+        /// </summary>
+        private static readonly string[] VALID_LANGUAGES = { "en", "de", "pl" };
+
+        /// <summary>
+        /// Determines whether a given language is valid for this project.
+        /// </summary>
+        public static bool IsValidLanguage(string language)
+        {
+            return Array.IndexOf(VALID_LANGUAGES, language) != -1;
+        }
+    }
+}
