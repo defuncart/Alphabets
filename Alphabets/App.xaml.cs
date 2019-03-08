5ad2f3ad3c7500f8c106e63f4cb41a47cce638a0
James Leahy <defuncart@gmail.com>
2019-03-08 11:31:13 +0100

    Feature/localization (#1)
    Basic localization system implemented.
    
    **Commits**
    
    * Added Plugin.Multilingual NuGet and hello world translations for en, de and pl.
    
    * Added Xam.Plugins.Settings and UserSettings wrapper. On first launch the user's device language is saved.
    
    * Moved localization resources to /Resources/Localization.
    
    * Created LocalizationManager which can be called from XAML (using updated TranslationExtension) or in code.

--- Alphabets/App.xaml.cs
+++ Alphabets/App.xaml.cs
@@ -1,4 +1,7 @@
-﻿using Alphabets.Views;
+﻿using Alphabets.Helpers;
+using Alphabets.Views;
+using DeFuncArt.Localization.Managers;
+using DeFuncArt.Localization.Helpers;
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 
@@ -9,8 +12,26 @@ namespace Alphabets
     {
         public App()
         {
-            //initialize all components and set the initial page
+            //initialize all components
             InitializeComponent();
+
+            //if this is the first launch, initialize the display language
+            if (!UserSettings.AlreadyLaunched)
+            {
+                //select the user's device language, if supported. otherwise the default ("en") will be kept
+                if (LocalizationHelper.IsValidLanguage(LocalizationManager.DeviceCultureInfo.TwoLetterISOLanguageName))
+                {
+                    UserSettings.Language = LocalizationManager.DeviceCultureInfo.TwoLetterISOLanguageName;
+                }
+                //set a flag that initial setup is complete
+                UserSettings.AlreadyLaunched = true;
+            }
+
+            //update localization depending on user's setting
+            LocalizationManager.SetLangauge(UserSettings.Language);
+
+
+            //set the initial page
             MainPage = new NavigationPage(new MainPage());
         }
     }
