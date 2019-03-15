b5698c415bdb4b02f958cd3b322527d6f31949db
James Leahy <defuncart@gmail.com>
2019-03-15 17:25:07 +0100

    Added basic audio manager (with one voice of polyphony).

---
+++ Alphabets/DeFuncArt/Audio/AudioManager.cs
@@ -0,0 +1,48 @@
+﻿using DeFuncArt.Helpers;
+using Plugin.SimpleAudioPlayer;
+using System.IO;
+
+namespace DeFuncArt.Audio
+{
+    /// <summary>
+    /// An audio manager which acts as a wrapper for the simple audio player plugin.
+    /// Only supports one voice of polyphony.
+    /// </summary>
+    public static class AudioManager
+    {
+        /// <summary>A resource prefix for where all audio files are located.</summary>
+        private const string RESOURCE_PREFIX = "Resources.Audio";
+
+        /// <summary>The default file format.</summary>
+        private const string DEFAULT_FILE_FORMAT = "wav";
+
+        /// <summary>The simple audio player.</summary>
+        private static readonly ISimpleAudioPlayer player;
+
+        /// <summary>
+        /// Initializes the <see cref="T:DeFuncArt.Audio.AudioManager"/> class.
+        /// </summary>
+        static AudioManager()
+        {
+            player = CrossSimpleAudioPlayer.Current;
+        }
+
+        /// <summary>
+        /// Play a specified filename with a given file format. Defaults to WAV.
+        /// </summary>
+        /// <param name="filename">The filename (i.e. SFX.answerCorrect).</param>
+        /// <param name="fileFormat">The file format.</param>
+        public static void Play(string filename, string fileFormat = DEFAULT_FILE_FORMAT)
+        {
+            //determine resource id
+            string resourceId = $"{FileHelper.AssemblyName}.{RESOURCE_PREFIX}.{filename}.{fileFormat}";
+
+            //get the stream
+            Stream stream = FileHelper.GetStream(resourceId);
+
+            //load the stream and play it
+            player.Load(stream);
+            player.Play();
+        }
+    }
+}
