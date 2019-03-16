5c485422e847aeda2ccce4050b73b468ece60d95
James Leahy <defuncart@gmail.com>
2019-03-16 10:55:55 +0100

    Updated LearnView to use a formatted string for pronounciation tip.

--- Alphabets/Views/Game/LearnView.xaml.cs
+++ Alphabets/Views/Game/LearnView.xaml.cs
@@ -2,6 +2,7 @@
 using DeFuncArt.Audio;
 using System;
 using Xamarin.Forms;
+using ResourceDictionary = Alphabets.Helpers.ResourceDictionary;
 
 namespace Alphabets.Views.Game
 {
@@ -37,10 +38,31 @@ namespace Alphabets.Views.Game
             //update ui
             letterLabel.Text = letter.Display;
             transliterationLabel.Text = letter.TransDisplay;
-            pronounciationLabel.Text = letter.PronounciationTip;
+            pronounciationLabel.FormattedText = FormatPronounciationTip(letter.PronounciationTip);
 
             //play letter pronounciation
             AudioManager.Play(letter.ResourceId, "mp3");
+        }
+
+        /// <summary>
+        /// Formats the prounciation tip into a FormattedString.
+        /// </summary>
+        private FormattedString FormatPronounciationTip(string pronounciationTip)
+        {
+            FormattedString formattedString = new FormattedString();
+
+            //loop through all sections of the string separated with *
+            string[] sections = pronounciationTip.Split('*');
+            for (int i = 0; i < sections.Length; i++)
+            {
+                Span span = new Span { Text = sections[i] };
+                //format all text between * to be a different color and bold
+                span.TextColor = i % 2 == 0 ? ResourceDictionary.GetColor("Color.MidGray") : ResourceDictionary.GetColor("Color.DarkGray1");
+                span.FontAttributes = i % 2 == 0 ? FontAttributes.None : FontAttributes.Bold;
+                formattedString.Spans.Add(span);
+            }
+
+            return formattedString;
         }
 
         #endregion
