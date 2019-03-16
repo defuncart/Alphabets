c9fa656dcc6b624ca55b40e77c555d9e311de1ac
James Leahy <defuncart@gmail.com>
2019-03-16 11:25:07 +0100

    Fixed bug in MainPage where lesson button could be pressed multiple times (and thus loading game multiple times).

--- Alphabets/Views/MainPage.xaml.cs
+++ Alphabets/Views/MainPage.xaml.cs
@@ -1,5 +1,4 @@
-﻿using System.Diagnostics;
-using Alphabets.Helpers;
+﻿using Alphabets.Helpers;
 using Alphabets.Managers;
 using DeFuncArt.Localization;
 using Xamarin.Forms;
@@ -21,7 +20,7 @@ namespace Alphabets.Views
             //initialize all components
             InitializeComponent();
 
-            //TODO
+            //TODO debug
             AlphabetManager.Log();
             CourseManager.Log();
 
@@ -46,12 +45,30 @@ namespace Alphabets.Views
         }
 
         /// <summary>
+        /// Callback before the page becomes visible.
+        /// </summary>
+        protected override void OnAppearing()
+        {
+            //call base implementaiton
+            base.OnAppearing();
+
+            //enabled all interaction
+            IsEnabled = true;
+        }
+
+        /// <summary>
         /// Callback when a lesson button was clicked.
         /// </summary>
         /// <param name="index">The button's index.</param>
         private void OnLesssonButtonClicked(int index)
         {
+            //disable all interaction
+            IsEnabled = false;
+
+            //update current lesson index
             UserSettings.CurrenLessonIndex = index;
+
+            //start lesson
             Navigation.PushModalAsync(new GamePage());
         }
     }
