a685862469032cf7250b58570493223fb3a33a87
James Leahy <defuncart@gmail.com>
2019-03-22 16:08:13 +0100

    Added TypingView.

---
+++ Alphabets/Views/Game/TypingView.xaml.cs
@@ -0,0 +1,18 @@
+﻿using Xamarin.Forms;
+
+namespace Alphabets.Views.Game
+{
+    /// <summary>
+    /// A view used when typing (ie transliterating a word).
+    /// </summary>
+    public partial class TypingView : ContentView
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="T:Alphabets.Views.Game.TypingView"/> class.
+        /// </summary>
+        public TypingView()
+        {
+            InitializeComponent();
+        }
+    }
+}
