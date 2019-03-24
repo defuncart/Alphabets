4c44241e0b7fc47769cb0aec5c4c47f9e6ffbe3f
James Leahy <defuncart@gmail.com>
2019-03-24 12:07:34 +0100

    Feature/alphabet view (#12)
    Overview:
    
    Added an AlphabetPage which the player can use to scroll through all letters in the alphabet. Updated database to contain 10 lessons covering all 33 letters in Georgian (NOTE only lessons 1 - 4 have typing exercises).
    
    Commits:
    
    * Renamed GamePage to LessonPage.
    
    * MainPage: lesson buttons are now laid out in a grid.
    
    * Added first ten lessons.
    
    Only lessons 1 - 4 have typing exercises.
    
    * Refactored letter flashcard from LearnView to its own view (LetterView).
    
    * Added AlphabetPage where each letter of the alphabet can be viewed as a flashcard.
    
    * Updated loca and letter tips.

---
+++ Alphabets/Views/AlphabetPage.xaml.cs
@@ -0,0 +1,101 @@
+﻿using Alphabets.Managers;
+using Alphabets.Models;
+using DeFuncArt.Extensions;
+using System;
+using Xamarin.Forms;
+
+namespace Alphabets.Views
+{
+    public partial class AlphabetPage : ContentPage
+    {
+        #region Variables
+
+        /// <summary>The current letter index.</summary>
+        private int letterIndex;
+
+        #endregion
+
+        #region Properties
+
+        /// <summary>The letters for the alphabet of the current course.</summary>
+        private Letter[] Letters => CourseManager.CurrentCourse.Alphabet.Letters;
+
+        #endregion
+
+        #region Methods
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="T:Alphabets.Views.AlphabetPage"/> class.
+        /// </summary>
+        public AlphabetPage()
+        {
+            //initialize all components
+            InitializeComponent();
+
+            //initialize ui
+            Title = CourseManager.CurrentCourse.Title;
+            leftButton.Text = "<";
+            rightButton.Text = ">";
+            UpdateView();
+        }
+
+        /// <summary>
+        /// Updates the view.
+        /// </summary>
+        private void UpdateView()
+        {
+            //display current letter
+            letterView.Setup(Letters[letterIndex]);
+
+            //determine if left/right buttons should be displayed
+            leftButton.SetEnabledVisible(letterIndex > 0);
+            rightButton.SetEnabledVisible(letterIndex < Letters.Length - 1);
+        }
+
+        /// <summary>
+        /// Changes the flashcard by either moving leftwards or rightwards.
+        /// </summary>
+        private void ChangeFlashcard(bool leftwardsDirection)
+        {
+            letterIndex = leftwardsDirection ? letterIndex - 1 : letterIndex + 1;
+            UpdateView();
+        }
+
+        #endregion
+
+        #region Callbacks
+
+        /// <summary>
+        /// Callback when the left button is clicked.
+        /// </summary>
+        private void OnLeftButtonClicked(object sender, EventArgs e)
+        {
+            ChangeFlashcard(leftwardsDirection: true);
+        }
+
+        /// <summary>
+        /// Callback when the right button is clicked.
+        /// </summary>
+        private void OnRightButtonClicked(object sender, EventArgs e)
+        {
+            ChangeFlashcard(leftwardsDirection: false);
+        }
+
+        /// <summary>
+        /// Callback when a swipe occured.
+        /// </summary>
+        private void OnSwiped(object sender, SwipedEventArgs e)
+        {
+            if (letterIndex > 0 && e.Direction == SwipeDirection.Right)
+            {
+                ChangeFlashcard(leftwardsDirection: true);
+            }
+            else if (letterIndex < Letters.Length - 1 && e.Direction == SwipeDirection.Left)
+            {
+                ChangeFlashcard(leftwardsDirection: false);
+            }
+        }
+
+        #endregion
+    }
+}
