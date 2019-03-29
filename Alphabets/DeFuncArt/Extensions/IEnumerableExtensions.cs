bebb77186f2081be2c78e828029100b8bc66fb8c
James Leahy <defuncart@gmail.com>
2019-03-29 13:55:34 +0100

    Added an extension method to shuffle an IEnumerable.

---
+++ Alphabets/DeFuncArt/Extensions/IEnumerableExtensions.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace DeFuncArt.Extensions
+{
+    /// <summary>A collection of extention methods.</summary>
+    public static partial class Extensions
+    {
+        /// <summary>
+        /// Shuffles the enumerable.
+        /// </summary>
+        public static IEnumerable<T> Shuffle<T>(this IEnumerable<T> enumerable)
+        {
+            return enumerable.OrderBy(x => random.Next()).ToList();
+        }
+    }
+}
